8a889859498a95ed4e15c5d16978271839fa37bf
yeasin <yeasin@qq.com>
2019-12-05 12:09:39 +0800

    xueqimiao

--- stream_api/src/com/xueqimiao/stream/StreamAPITest.java
+++ stream_api/src/com/xueqimiao/stream/StreamAPITest.java
@@ -5,8 +5,11 @@ import org.junit.Test;
 import javax.swing.plaf.synth.SynthSpinnerUI;
 import java.lang.reflect.Array;
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 /**
  * @version v1.0
@@ -19,10 +22,10 @@ public class StreamAPITest {
     @Test
     public void test01() {
         List<String> list = Arrays.asList(
-                "g","k","f","k","g","b"
+                "g", "k", "f", "k", "g", "b"
         );
         List<String> li = list.stream().filter(li1 -> list.stream().filter((li2 -> li2.equals(li1))).count() > 1l)
-                .collect(Collectors.toList());
+                .collect(toList());
         System.out.println(li);
     }
 
@@ -48,7 +51,7 @@ public class StreamAPITest {
     //2.取出前2个user01s的姓名
     @Test
     public void test02() {
-        List<String> nameList = users01.stream().map(User::getName).limit(2).collect(Collectors.toList());
+        List<String> nameList = users01.stream().map(User::getName).limit(2).collect(toList());
         System.out.println(nameList);
         Set<String> nameSet = users01.stream().map(User::getName).collect(Collectors.toSet());
         System.out.println(nameSet);
@@ -60,14 +63,14 @@ public class StreamAPITest {
     @Test
     public void test03() {
         List<User> userList = users01.stream().filter(user1 -> users01.stream().filter((user2 -> user2.getAge().equals(user1.getAge()))).count() > 1l)
-                .collect(Collectors.toList());
+                .collect(toList());
 
         userList.stream().forEach((user) -> {
             System.out.println(user.getName() + ":" + user.getAge());
         });
         Map<Object, Object> str = new HashMap<>();
         for (User user : userList) {
-            List<User> collect = userList.stream().filter(d -> d.getAge().equals(user.getAge())).collect(Collectors.toList());
+            List<User> collect = userList.stream().filter(d -> d.getAge().equals(user.getAge())).collect(toList());
             str.put(user.getAge(), collect.size());
         }
         for (Object key : str.keySet()) {
@@ -85,9 +88,9 @@ public class StreamAPITest {
     @Test
     public void test04() {
         //取出所有users02的id
-        List<Integer> user02Ids = users02.stream().map(User::getId).collect(Collectors.toList());
+        List<Integer> user02Ids = users02.stream().map(User::getId).collect(toList());
         System.out.println(user02Ids);
-        List<User> userList = users01.stream().filter(user1 -> !user02Ids.stream().anyMatch(user02Id -> user1.getId().equals(user02Id))).collect(Collectors.toList());
+        List<User> userList = users01.stream().filter(user1 -> !user02Ids.stream().anyMatch(user02Id -> user1.getId().equals(user02Id))).collect(toList());
         userList.stream().forEach((user) -> {
             System.out.println(user.getName() + ":" + user.getAge());
         });
@@ -110,26 +113,26 @@ public class StreamAPITest {
     public void test06() {
 
         //根据年龄逆序
-        List<User> userList = users01.stream().sorted(Comparator.comparing(User::getBirthday).reversed()).collect(Collectors.toList());
+        List<User> userList = users01.stream().sorted(Comparator.comparing(User::getBirthday).reversed()).collect(toList());
         userList.stream().forEach((user) -> {
-            System.out.println(user.getName() + ":" + user.getAge()+":"+user.getBirthday());
+            System.out.println(user.getName() + ":" + user.getAge() + ":" + user.getBirthday());
         });
         System.out.println("--------------------------");
         //根据年龄自然顺序
-        List<User> userList1 = userList.stream().sorted(Comparator.comparing(User::getBirthday)).collect(Collectors.toList());
+        List<User> userList1 = userList.stream().sorted(Comparator.comparing(User::getBirthday)).collect(toList());
         userList1.stream().forEach((user) -> {
-            System.out.println(user.getName() + ":" + user.getAge()+":"+user.getBirthday());
+            System.out.println(user.getName() + ":" + user.getAge() + ":" + user.getBirthday());
         });
     }
 
     //7.去掉users01name重复的只保留一个
     @Test
     public void test07() {
-       List<User>  userList = users01.stream().collect(
+        List<User> userList = users01.stream().collect(
                 Collectors.collectingAndThen(Collectors.toCollection(() -> new TreeSet<>(Comparator.comparing(o -> o.getName()))),
                         ArrayList::new));
         userList.stream().forEach((user) -> {
-            System.out.println(user.getName() + ":" + user.getAge()+":"+user.getBirthday());
+            System.out.println(user.getName() + ":" + user.getAge() + ":" + user.getBirthday());
         });
     }
 
@@ -212,49 +215,156 @@ public class StreamAPITest {
      */
     //12.累加数字
     @Test
-    public void test12(){
-        List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+    public void test12() {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         Integer sum = list.stream()
                 .reduce(0, (x, y) -> x + y);
         System.out.println(sum);
     }
 
     List<Student> stuList = Arrays.asList(
-            new Student(1,"张三",1),
-            new Student(2,"李四",2),
-            new Student(3,"李飞",2)
+            new Student(1, "张三", 1),
+            new Student(2, "李四", 2),
+            new Student(3, "李飞", 2)
     );
 
     List<Grade> gradeList = Arrays.asList(
-            new Grade(1,"大一"),
-            new Grade(2,"大二")
+            new Grade(1, "大一"),
+            new Grade(2, "大二")
     );
 
     //13.将Student信息注入到相应的Grade里面的studentList中
     @Test
-    public void test13(){
-        gradeList.stream().forEach((grade)->{
-            List<Student> students = stuList.stream().filter((stu) -> stu.getGradeId().equals(grade.getGradeId())).collect(Collectors.toList());
+    public void test13() {
+        gradeList.stream().forEach((grade) -> {
+            List<Student> students = stuList.stream().filter((stu) -> stu.getGradeId().equals(grade.getGradeId())).collect(toList());
             grade.setStudentList(students);
         });
         gradeList.stream().forEach(grade -> {
-            System.out.println(grade.getGradeName()+"---");
-            grade.getStudentList().stream().forEach(stu->{
+            System.out.println(grade.getGradeName() + "---");
+            grade.getStudentList().stream().forEach(stu -> {
                 System.out.println(stu.getStuName());
             });
         });
     }
 
-    /*
-    * List<Integer> intList = new ArrayList<>();
-intList.add(1);
-intList.add(3);
-intList.add(4);
-List<String> stringList = intList.stream().map(String::valueOf).collect(Collectors.toList());
+    //14.字符串数字集合与整数集合互转
+    @Test
+    public void test14() {
+        List<Integer> intList = new ArrayList<>();
+        intList.add(1);
+        intList.add(3);
+        intList.add(4);
+        List<String> intToStrList = intList.stream().map(String::valueOf).collect(toList());
+        intToStrList.stream().forEach(str -> {
+            System.out.println(str);
+        });
 
+        List<String> stringList = new ArrayList<>();
+        stringList.add("10");
+        stringList.add("20");
+        stringList.add("30");
+        List<String> stringToIntList = stringList.stream().map(String::valueOf).collect(toList());
+        stringToIntList.stream().forEach(num -> {
+            System.out.println(num);
+        });
+    }
 
-    * */
+    List<String> list1 = Arrays.asList(
+            "张三", "李四", "李刚", "隋通"
+    );
 
-    //https://www.cnblogs.com/heqiyoujing/p/9859144.html
+    List<String> list2 = Arrays.asList(
+            "隋凯", "李通", "张三", "李刚"
+    );
 
+    //15.取list1与list2的交集
+    @Test
+    public void test15() {
+        List<String> intersection = list1.stream().filter(item -> list2.contains(item)).collect(toList());
+        System.out.println("---得到交集---");
+        intersection.parallelStream().forEach(System.out::println);
+    }
+
+    //16.取list1与list2的差集
+    @Test
+    public void test16() {
+        List<String> reduce1 = list1.stream().filter(item -> !list2.contains(item)).collect(toList());
+        System.out.println("---得到差集 reduce1 (list1 - list2)---");
+        reduce1.parallelStream().forEach(System.out::println);
+    }
+
+    //17.取list1与list2的差集
+    @Test
+    public void test17() {
+        List<String> reduce2 = list2.stream().filter(item -> !list1.contains(item)).collect(toList());
+        System.out.println("---得到差集 reduce2 (list2 - list1)---");
+        reduce2.parallelStream().forEach(System.out::println);
+    }
+
+    //18.取list1和list2的并集
+    @Test
+    public void test18() {
+        List<String> listAll = list1.parallelStream().collect(toList());
+        List<String> listAll2 = list2.parallelStream().collect(toList());
+        listAll.addAll(listAll2);
+        System.out.println("---得到并集 listAll---");
+        listAll.parallelStream().forEach(System.out::println);
+        // 去重并集
+        List<String> listAllDistinct = listAll.stream().distinct().collect(toList());
+        System.out.println("---得到去重并集 listAllDistinct---");
+        listAllDistinct.parallelStream().forEach(System.out::println);
+        System.out.println("---原来的List1---");
+        list1.parallelStream().forEach(System.out::println);
+        System.out.println("---原来的List2---");
+        list2.parallelStream().forEach(System.out::println);
+    }
+
+    List<String> list = Arrays.asList(
+            "b", "g", "d", "d", "t", "j", "d", "a", "c", "f", "a", "b", "s", "w", "w", "b"
+    );
+
+    //19.统计集合重复元素出现次数，并且去重返回hashmap
+    @Test
+    public void test19() {
+        Map<String, Long> map = list.stream().
+                collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        System.out.println(map);
+        //由于hashmap无序，所以在排序放入LinkedHashMap里(key升序)
+        Map<String, Long> sortMap = new LinkedHashMap<>();
+        map.entrySet().stream().sorted(Map.Entry.comparingByKey()).
+                forEachOrdered(e -> sortMap.put(e.getKey(), e.getValue()));
+        System.out.println(sortMap);
+        //获取排序后map的key集合
+        List<String> keys = new LinkedList<>();
+        sortMap.entrySet().stream().forEachOrdered(e -> keys.add(e.getKey()));
+        System.out.println(keys);
+        //获取排序后map的value集合
+        List<Long> values = new LinkedList<>();
+        sortMap.entrySet().stream().forEachOrdered(e -> values.add(e.getValue()));
+        System.out.println(values);
+    }
+
+    //20.模拟数据库模糊查询users01
+    @Test
+    public void test20(){
+        users01.stream().forEach(u->{
+            System.out.println(u.getAge());
+        });
+        System.out.println("----------------------");
+        users01 = users01.stream().filter(vo -> {
+            return  vo.getName().contains("三")||vo.getBirthday().contains("2018");
+        }).collect(toList());
+        users01.stream().forEach(u->{
+            System.out.println(u.getName()+":"+u.getBirthday());
+        });
+    }
+
+    //21.stream循环拿到下标
+    @Test
+    public void test21(){
+        Stream.iterate(0, i -> i + 1).limit(users01.size()).forEach(index -> {
+           System.out.println(index);
+        });
+    }
 }
